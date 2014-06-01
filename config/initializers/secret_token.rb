ff9f416e7778a644faa2ee93d1cb94ac14dde50f
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-05-31 22:12:43 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PocketSOHO::Application.config.secret_key_base = 'bb2e542cc7edfc14a4edb07dac2349bfc6f324d373fcbef595d11bc6aa398b25bba7ce751c309d95048bd7dbbcdb7de6ad2a131c08ead4b0e841071bac700b0b'
