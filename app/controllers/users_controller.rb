c1944a5b645454ed9ba72e679aa7eb3332a4523b
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-06-08 02:01:41 -0700

    Creates variables for signed in user, their customers + businesses

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   def home
+    @user = User::BusinessOwner.find(session[:user_id]) || User::Customer.find(session[:user_id])
     @businesses = Business.all #change to find_by_user_id
-    @customers = User::Customer.all #change to find_by_business_id
+    if @user.type == "Business Owner"
+      @customers = @user.businesses.users.find_by_type("Customer") #verify this is correct
   end
 
   def new
