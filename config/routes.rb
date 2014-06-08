f810877174ee0504e16e34bc3c287b7c29db38bf
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-06-07 18:45:45 -0700

    Spaces out lines in routes.rb for readability

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 PocketSOHO::Application.routes.draw do
-  scope module: 'welcome' do
-    root 'welcome#index'
-    resources :sessions, only: [:new, :create, :destroy]
-    get '/user/home', to: 'users#home', as: 'user homepage'
-  end
+  root 'welcome#index'
 
-  resources :businesses, only: [:create, :show, :edit, :update, :destroy]
-  resources :users, only: [:create, :show, :edit, :update, :destroy] do
+  resources :sessions, only: [:new, :create, :destroy]
+  get '/user/home', to: 'users#home', as: :user_homepage
+
+  resources :businesses, only: [:new, :create, :show, :edit, :update, :destroy]
+
+  resources :users, only: [:new, :create, :show, :edit, :update, :destroy] do
     resources :messages, only: [:index, :new, :create, :show, :destroy]
   end
 
