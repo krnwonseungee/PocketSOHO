8473b3ea176610d6e1b35db6cfb8f7cfd668c0d6
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-06-07 19:20:15 -0700

    Corrects creates business and users migration

--- db/migrate/20140607164722_create_businesses_and_users.rb
+++ db/migrate/20140607164722_create_businesses_and_users.rb
@@ -1,6 +1,6 @@
 class CreateBusinessesAndUsers < ActiveRecord::Migration
   def change
-    create_table :users do |t|
+    create_table :businesses do |t|
       t.string :name
       t.string :location
       t.string :description
@@ -10,20 +10,19 @@ class CreateBusinessesAndUsers < ActiveRecord::Migration
       t.timestamps
     end
 
-    create_table :business_owners do |t|
+    create_table :users do |t|
       t.string :first_name
       t.string :last_name
       t.string :location
-      t.string :emali
+      t.string :email
       t.string :image_url
-      t.belongs_to :business
 
       t.timestamps
     end
 
     create_table :businesses_users, id: false do |t|
       t.belongs_to :business
-      t.belongs_to :business_owner
+      t.belongs_to :user
     end
   end
 end
