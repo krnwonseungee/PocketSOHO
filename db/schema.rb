ceda1ad8098d3a21a8dac6ddb021822d8d88cb25
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-06-16 00:39:32 -0700

    Adds sample biz & message to seed data; changes to schema

--- db/schema.rb
+++ db/schema.rb
@@ -22,22 +22,24 @@ ActiveRecord::Schema.define(version: 20140607164722) do
     t.string   "description"
     t.string   "slogan"
     t.string   "image_url"
+    t.integer  "business_owner_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "businesses_users", id: false, force: true do |t|
-    t.integer "business_id"
-    t.integer "user_id"
   end
 
   create_table "messages", force: true do |t|
     t.text     "text"
-    t.integer  "business_id"
     t.integer  "business_owner_id"
     t.integer  "customer_id"
+    t.integer  "business_id"
+    t.integer  "thread_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "threads", id: false, force: true do |t|
+    t.integer "business_owner_id"
+    t.integer "customer_id"
   end
 
   create_table "users", force: true do |t|
