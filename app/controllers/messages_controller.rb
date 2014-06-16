88f24960984cb77a4393c3c85d10fbb73b50a55e
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-06-15 18:50:58 -0700

    Adds messages#show view and controller methods

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,22 @@
 class MessagesController < ApplicationController
 
   def index
-    @user = BusinessOwner.find(session[:user_id]) || Customer.find(session[:user_id])
+    @user = User.find_by_email(session[:email])
+    @messages = @user.messages
   end
 
   def new
+    @message = Message.new
+  end
 
+  def show
+    @user = User.find_by_email(session[:email])
+    @message = Message.find(params[:id])
+    if @user.type == "BusinessOwner"
+      @thread_messages = Message.where("business_owner_id = ? AND customer_id = ?", @user.id, @message.customer_id )
+    else
+      @thread_messages = Message.where("business_owner_id = ? AND customer_id = ?", @message.business_owner_id, @user.id )
+    end
   end
 
   def create
