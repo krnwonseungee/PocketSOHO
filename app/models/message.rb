8304135c283daa96b468b74ebb8dc693132574fe
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-06-16 00:33:54 -0700

    Adds associations between customer & biz owner for Message & Thread

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,9 @@
 class Message < ActiveRecord::Base
-  belongs_to :businesses
-  belongs_to :threads
+  belongs_to :business_owner
+  belongs_to :customer
+  belongs_to :business
+  belongs_to :thread
+
+  def initialize
+  end
 end
--- app/models/thread.rb
+++ app/models/thread.rb
@@ -1,3 +1,5 @@
 class Thread < ActiveRecord::Base
+  has_one :customer
+  has_one :business_owner
   has_many :messages
 end
