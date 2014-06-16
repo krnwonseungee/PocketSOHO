beb60b378cf99255b02fd5dc26829bce3dff15b8
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-06-15 18:49:48 -0700

    Adds threads table. has many messages

---
+++ db/migrate/20140607164022_create_messages_and_threads.rb
@@ -0,0 +1,15 @@
+class CreateMessagesAndThreads < ActiveRecord::Migration
+  def change
+    create_table :messages do |t|
+      t.text :text
+      t.belongs_to :business
+      t.belongs_to :thread
+      t.timestamps
+    end
+
+    create_table :threads, id: false do |t|
+      t.belongs_to :business_owner
+      t.belongs_to :customer
+    end
+  end
+end
