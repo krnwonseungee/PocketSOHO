6d7ae544a71e98e413350b080b322508dad58765
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-06-16 22:32:27 -0700

    Adds initialize method to thread model

--- app/models/thread.rb
+++ app/models/thread.rb
@@ -2,4 +2,11 @@ class Thread < ActiveRecord::Base
   has_one :customer
   has_one :business_owner
   has_many :messages
+
+  def initialize(*args)
+    args_hash = args.first
+    @customer_id = args_hash[:customer_id]
+    @business_owner_id = args_hash[:business_owner_id]
+  end
+
 end
