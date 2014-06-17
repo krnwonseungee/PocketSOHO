c8a6e2e3f5d422fa3ea18b76272070ca36faf021
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-06-16 22:31:53 -0700

    Adds initialize method in model to create new thread when created

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,6 +4,19 @@ class Message < ActiveRecord::Base
   belongs_to :business
   belongs_to :thread
 
-  def initialize
+  def initialize(*args)
+    args_hash = args.first
+    @text = args_hash[:text]
+    @customer_id = args_hash[:customer_id]
+    @business_owner_id = args_hash[:business_owner_id]
+    @business_id = args_hash[:business_id]
+    @thread_mssages = Message.where("business_owner_id = ? AND customer_id = ?", args_hash[:business_owner_id], args_hash[:customer_id] )
+    if @thread_messages == nil
+      new_thread = Thread.new( customer_id: args_hash[:customer_id], business_owner_id: args_hash[:business_owner_id] )
+      thread_id = new_thread.id
+    else
+      @thread_id = @thread_messages.first.thread_id
+    end
   end
+
 end
