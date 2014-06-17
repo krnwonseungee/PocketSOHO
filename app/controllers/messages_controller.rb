f92f2d294c72213e43371cef41cd49924ac1fa99
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-06-16 22:30:42 -0700

    Adds messages#create method

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,11 +6,11 @@ class MessagesController < ApplicationController
   end
 
   def new
-    @message = Message.new
   end
 
   def show
     @user = User.find(session[:user_id])
+    @new_message = Message.new
     @message = Message.find(params[:id])
     if @user.type == "BusinessOwner"
       @thread_messages = Message.where("business_owner_id = ? AND customer_id = ?", @user.id, @message.customer_id )
@@ -20,7 +20,13 @@ class MessagesController < ApplicationController
   end
 
   def create
-
+    @user = User.find(session[:user_id])
+    if @user.type == "BusinessOwner"
+      Message.create( text: params[:message][:text], customer_id: 2, business_owner_id: @user.id )
+    else
+      Message.create( text: params[:message][:text], customer_id: @user.id, business_owner_id: 1 )
+    end
+    redirect_to user_messages_path(@user.id)
   end
 
   def destroy
