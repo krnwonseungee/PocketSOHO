15072191455923cee1df746704d61a11ac2c4c1d
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-06-18 00:46:32 -0700

    Now assigns conversation id upon Message.create   * removes puts statements

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,15 +4,15 @@ class Message < ActiveRecord::Base
   belongs_to :business
   belongs_to :conversation
 
-  attr_accessor :conversation_id, :customer_id, :business_owner_id
-  after_create :assign_thread_id
+  before_create :assign_thread_id
 
   def assign_thread_id
-    puts "!!!#{self.customer_id}"
-    puts "!!!#{self.business_owner_id}"
-    if !Message.where("business_owner_id = ? AND customer_id = ?", self.business_owner_id, self.customer_id ).exists?
-      new_conversation = Conversation.create( customer_id: self.customer_id, business_owner_id: self.business_owner_id )
-      conversation_id = new_conversation.id
+    if Message.where("business_owner_id = ? AND customer_id = ?", self.business_owner_id, self.customer_id ).empty?
+      new_conversation = Conversation.create( business_owner_id: self.business_owner_id, customer_id: self.customer_id )
+      print new_conversation
+      self.conversation_id = new_conversation.id
+    else
+      self.conversation_id = Message.where("business_owner_id = ? AND customer_id = ?", self.business_owner_id, self.customer_id ).first.conversation_id
     end
   end
 
