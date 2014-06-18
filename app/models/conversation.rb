38751115c041cb727e7357cd39eebe67cec1a129
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-06-18 00:44:47 -0700

    Removes initialize method (overwrites create method)

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -3,10 +3,4 @@ class Conversation < ActiveRecord::Base
   has_one :business_owner
   has_many :messages
 
-  def initialize(*args)
-    args_hash = args.first
-    @customer_id = args_hash[:customer_id]
-    @business_owner_id = args_hash[:business_owner_id]
-  end
-
 end
