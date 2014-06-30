370cb12c38381622bc1493b92c9438efee2e8ce5
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-06-30 12:55:35 -0700

    Adds PgSearch module. wip

--- app/models/message_searcher.rb
+++ app/models/message_searcher.rb
@@ -1,5 +1,7 @@
-class MessageSearcher
+class MessageSearcher < Message
+  include PgSearch
+  multisearchable :against => [:first_name, ]
   def retrieve_relevant_messages(search_term)
-    puts search_term
+    PgSearch.multisearch(search_term)
   end
 end
