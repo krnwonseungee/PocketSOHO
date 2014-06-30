7042c476f349e147634f3acf0d8c7caa15dbb8af
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-06-29 18:28:17 -0700

    Displays unread messages on welcome screen (welcome#home)   * changes to controller & view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,20 @@ class WelcomeController < ApplicationController
     if session[:user_id]
       @user = User.find(session[:user_id])
       @businesses = Business.all #change to find_by_user_id
+      @unread_conversations = Array.new
+
+      if @user.type == "BusinessOwner"
+        Conversation.where("business_owner_id = ? AND seen_by_business_owner = ?", @user.id, false ).each do |thread|
+          @unread_conversations << thread.messages.last
+        end
+
+      else
+        Conversation.where("customer_id = ? AND seen_by_customer = ?", @user.id, false ).each do |thread|
+          @unread_conversations << thread.messages.last
+        end
+      end
+
+      @unread_conversations.sort_by!{ |msg| msg.updated_at }.reverse!
       render "home"
     else
       render "info"
