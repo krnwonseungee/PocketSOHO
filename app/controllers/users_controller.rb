5d5c6aed15882f1c570d0bb2e9c50ec4fa0ba855
Janet Yi <janetyi@block9.public.monkeybrains.net>
2014-07-19 19:23:08 -0700

    adds settings route in users controller. wip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,4 +19,9 @@ class UsersController < ApplicationController
   def destroy
 
   end
+
+  def settings
+    @user = User.find(session[:user_id])
+    @businesses = Business.where("business_owner_id = ?", @user.id )
+  end
 end
