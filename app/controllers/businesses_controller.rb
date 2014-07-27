e3e895d97f708bc62cafc1ec438e5b6f4dae38ae
Janet Yi <janetyi@block9.public.monkeybrains.net>
2014-07-27 15:14:51 -0700

    Adds business controller pages

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,18 +1,19 @@
 class BusinessesController < ApplicationController
   def new
+    @business = Business.find(params[:id])
 
   end
 
   def create
-
+    @business = Business.find(params[:id])
   end
 
   def edit
-
+    @business = Business.find(params[:id])
   end
 
   def show
-
+    @business = Business.find(params[:id])
   end
 
   def update
