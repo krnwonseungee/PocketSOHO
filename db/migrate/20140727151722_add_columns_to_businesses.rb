13f477addbe92612a6aa279a4bbbd4e1eb9ff124
Janet Yi <janetyi@block9.public.monkeybrains.net>
2014-07-27 15:23:20 -0700

    Adds add. columsn to businesses

---
+++ db/migrate/20140727151722_add_columns_to_businesses.rb
@@ -0,0 +1,8 @@
+class AddColumnsToBusinesses < ActiveRecord::Migration
+  def change
+    add_column :businesses, :city, :string
+    add_column :businesses, :state, :string
+    remove_column :businesses, :description, :string
+    add_column :businesses, :description, :text
+  end
+end
