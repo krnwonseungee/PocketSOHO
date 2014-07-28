50aa053c23763710628fc3a1c885ada702cf5795
Janet Yi <janetyi@block9.public.monkeybrains.net>
2014-07-27 19:18:34 -0700

    Adds appointments controller

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,40 @@
+class AppointmentsController < ApplicationController
+
+  def index
+    @user = User.find(session[:user_id])
+      if @user.type == "BusinessOwner"
+          @appointments = @appointments = Appointment.where( "business_owner_id = ?", @user.id )
+      else
+          @appointments = Appointment.where( "customer_id = ?", @user.id )
+      end
+
+  end
+
+  def new
+    @user = User.find(session[:user_id])
+  end
+
+  def create
+    @user = User.find(session[:user_id])
+  end
+
+  def edit
+    @user = User.find(session[:user_id])
+    @business = Business.find(params[:id])
+  end
+
+  def show
+    @user = User.find(session[:user_id])
+    @business = Business.find(params[:id])
+    @biz_owner = User.find(@business.business_owner_id)
+  end
+
+  def update
+    @user = User.find(session[:user_id])
+
+  end
+
+  def destroy
+    @user = User.find(session[:user_id])
+  end
+end
