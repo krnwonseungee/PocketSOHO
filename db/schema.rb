3caafe4d7f1506d8366d646965e8b4eb01c33930
Janet Yi <janetyi@block9.public.monkeybrains.net>
2014-07-27 18:18:05 -0700

    Corresponding schema changes   * added appointment table/model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140727161622) do
+ActiveRecord::Schema.define(version: 20140727181422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appointments", force: true do |t|
+    t.text     "text"
+    t.integer  "customer_id"
+    t.integer  "business_owner_id"
+    t.integer  "business_id"
+    t.text     "notes"
+    t.date     "date"
+    t.time     "time"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "businesses", force: true do |t|
     t.string   "name"
