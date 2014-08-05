bb50a2c25c9a1168989a5bc66f8281e6eb97f64f
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-08-04 23:07:38 -0700

    Changes to invoice controller routes

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,23 +1,37 @@
 class InvoicesController < ApplicationController
 
   def index
+    @user = User.find(session[:user_id])
+      if @user.type == "BusinessOwner"
+        ##FIX
+          @invoices = Invoice.where( "business_id = ?", @user.businesses.first.id )
+      else
+          @invoices = Invoice.where( "customer_id = ?", @user.id )
+      end
+      @invoice_dates = @invoices.pluck( :due_date )
   end
 
   def create
+    @user = User.find(session[:user_id])
   end
 
   def new
+    @user = User.find(session[:user_id])
   end
 
   def show
+    @user = User.find(session[:user_id])
   end
 
   def edit
+    @user = User.find(session[:user_id])
   end
 
   def update
+    @user = User.find(session[:user_id])
   end
 
   def destroy
+    @user = User.find(session[:user_id])
   end
 end
