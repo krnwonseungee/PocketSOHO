5f1dd175ba19c9d12f1b00af90911bfa0ab069ff
Janet Yi <janetyi@block9.public.monkeybrains.net>
2014-08-09 15:19:40 -0700

    Going back to jquery for messaging. angular is saved in inboxold

--- app/assets/javascripts/inbox.js
+++ app/assets/javascripts/inbox.js
@@ -1,12 +1,35 @@
-//isolates user id by parsing through URL
-var userId = window.location.href.split("/")[4]
-// console.log(a=userId)
+Inbox = {}
+Inbox.controller = function(){}
 
-// creates inbox controller
-app.controller('inboxController', ['$scope', '$http', function($scope, $http){
 
-  $http.get("/users/" + userId + "/conversations.json").success(function(data){
-    $scope.messages = data;
-    console.log(b=$scope.messages)
-  })
-}])
+Inbox.controller.prototype = {
+  bind: function(){
+    $("#convos_results").keyup(function(e){
+      e.preventDefault();
+      var inboxSearchTerm = $( '#convos_results' ).val();
+      // console.log(inboxSearchTerm)
+      inboxcontroller.searchBarSubmit(inboxSearchTerm);
+    })
+  },
+
+  searchBarSubmit: function(searchTerm){
+    $.ajax({
+      type: 'post',
+      beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
+      url: "/conversations/results",
+      data: { search_term: searchTerm },
+      dataType: 'json',
+      success: function(){ console.log("success") },
+      error: function(){ console.log("error") },
+    }).done(function(data){
+      console.log(data)
+    })
+  },
+}
+
+$(document).ready(function(){
+  inboxcontroller.bind();
+})
+
+inboxcontroller = new Inbox.controller
+//
--- app/assets/javascripts/inboxold.js
+++ app/assets/javascripts/inboxold.js
@@ -1,35 +1,12 @@
-Inbox = {}
-Inbox.controller = function(){}
+//isolates user id by parsing through URL
+var userId = window.location.href.split("/")[4]
+// console.log(a=userId)
 
+// creates inbox controller
+app.controller('inboxController', ['$scope', '$http', function($scope, $http){
 
-Inbox.controller.prototype = {
-  bind: function(){
-    $("#convos_results").keyup(function(e){
-      e.preventDefault();
-      var inboxSearchTerm = $( '#convos_results' ).val();
-      // console.log(inboxSearchTerm)
-      inboxcontroller.searchBarSubmit(inboxSearchTerm);
-    })
-  },
-
-  searchBarSubmit: function(searchTerm){
-    $.ajax({
-      type: 'post',
-      beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
-      url: "/conversations/results",
-      data: { search_term: searchTerm },
-      dataType: 'json',
-      success: function(){ console.log("success") },
-      error: function(){ console.log("error") },
-    }).done(function(data){
-      console.log(data)
-    })
-  },
-}
-
-$(document).ready(function(){
-  inboxcontroller.bind();
-})
-
-inboxcontroller = new Inbox.controller
-//
+  $http.get("/users/" + userId + "/conversations.json").success(function(data){
+    $scope.messages = data;
+    console.log(b=$scope.messages)
+  })
+}])
