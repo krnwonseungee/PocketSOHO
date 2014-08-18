717b1746ccae77ff7a84bd7dd3dcbdf0af952c0a
Janet Yi <janetyi@block9.public.monkeybrains.net>
2014-08-17 22:11:58 -0700

    Redirects to edit user path if user first name & last name not filled

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,10 +4,14 @@ class WelcomeController < ApplicationController
 
   def index
     if user_signed_in?
-      @businesses = Business.all #change to find_by_user_id
+      if current_user.first_name == nil && current_user.last_name == nil
+        redirect_to edit_user_path(current_user)
+        return ""
+      end
       @unread_conversations = Array.new
 
       if @user.type == "BusinessOwner"
+        @businesses = Business.where( "business_owner_id = ?", @user.id )
         Conversation.where("business_owner_id = ? AND seen_by_business_owner = ?", @user.id, false ).each do |thread|
           @unread_conversations << thread.messages.last
         end
@@ -15,6 +19,7 @@ class WelcomeController < ApplicationController
         @appointments = Appointment.where( "business_owner_id = ?", @user.id ).take(3)
 
       else
+        @businesses = Business.where( "id = ?", @user.business_id ) # change for multiple businesses
         Conversation.where("customer_id = ? AND seen_by_customer = ?", @user.id, false ).each do |thread|
           @unread_conversations << thread.messages.last
         end
