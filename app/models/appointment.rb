3d12ea6cd09fbac8cc025de4c5836c4b50f0b70f
Janet Yi <janetyi90@gmail.com>
2014-09-28 10:36:14 -0700

    Creates callback method for appointment model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,4 +3,16 @@ class Appointment < ActiveRecord::Base
   belongs_to :business_owner
   belongs_to :business
   belongs_to :invoice
+
+  before_create :assign_invoice_id
+
+  def assign_invoice_id
+
+  end
 end
+
+=begin
+
+
+
+=end
