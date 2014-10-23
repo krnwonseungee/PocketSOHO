e10d3b6e3455b22826ea7f38b0fbe0f4af8106f4
Janet Yi <janetyi90@gmail.com>
2014-10-22 21:10:33 -0700

    Fixing devise log out error

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ PocketSOHO::Application.routes.draw do
   # devise_scope :user do
   #   get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   # end
-
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root 'welcome#index'
 
