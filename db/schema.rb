1d84634c2c5665eec33047d9828289e2405f07b7
Janet Yi <janetyi90@gmail.com>
2015-02-23 21:35:07 -0800

    Adds invited column for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150104171400) do
+ActiveRecord::Schema.define(version: 20150223201000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -107,6 +107,7 @@ ActiveRecord::Schema.define(version: 20150104171400) do
     t.string   "provider"
     t.string   "uid"
     t.string   "twitter_handle"
+    t.boolean  "invited",                default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
